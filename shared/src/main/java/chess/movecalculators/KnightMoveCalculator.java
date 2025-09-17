d29c54ea819e044315ff50baa7b6d970bd4bd5a0
Garrett-Webster <garrettreeds@gmail.com>
2025-09-17 13:46:38 -0600

    Through Knight moves

--- shared/src/main/java/chess/movecalculators/KnightMoveCalculator.java
+++ shared/src/main/java/chess/movecalculators/KnightMoveCalculator.java
@@ -4,7 +4,19 @@ import chess.ChessBoard;
 import chess.ChessPosition;
 
 public class KnightMoveCalculator extends MoveCalculator {
+    int[][] knightMoveOffsets = {
+            {2, 1},
+            {2, -1},
+            {-2, 1},
+            {-2, -1},
+            {1, 2},
+            {1, -2},
+            {-1, 2},
+            {-1, -2},
+    };
+
     public KnightMoveCalculator(ChessPosition position, ChessBoard board) {
         super(position, board);
+        addMovesFromOffsets(knightMoveOffsets);
     }
 }
